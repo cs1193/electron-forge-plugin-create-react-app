a2ef1eb51ef8b9e15b0242a28f8c4ce0885c6146
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2021-06-29 23:30:25 +0530

    fix: dev: develop: moved changes to webpack

--- src/CreateReactAppPlugin.ts
+++ src/CreateReactAppPlugin.ts
@@ -78,7 +78,7 @@ export default class CreateReactAppPlugin extends PluginBase<ICreateReactAppPlug
       installYarnModules(this.projectDir, module.path);
       copyBuildData(this.projectDir, module.name, module.path);
 
-      this.definesData[defineName] = `.create-react-app/${module.name}/index.html`;
+      this.definesData[defineName] = `\`file://$\{path.resolve(__dirname, '..', '.create-react-app', '${module.name}','index.html')}\``;
       resolve(defineName);
     } catch (e) {
       reject(e);
